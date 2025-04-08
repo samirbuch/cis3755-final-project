526e062abaff08998506df37bec685203ac54868
samirbuch <samir.buch@temple.edu>
2025-04-07 20:30:31 -0400

    home page: animate title and subtitle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,48 @@
+"use client";
+
 import Head from "next/head";
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+// import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import { Center, Flex, Text, Title } from "@mantine/core";
+import { Flex, Text, Title } from "@mantine/core";
 import CenteredOnPage from "@/components/CenteredOnPage";
+import { useEffect, useRef } from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { createScope, animate, type Scope, createTimeline } from "animejs";
 
 export default function Home() {
+  const root = useRef<HTMLDivElement>(null);
+  const scope = useRef<Scope>(null);
+
+  useEffect(() => {
+    if (!root.current) return;
+
+    scope.current = createScope({ root: root.current }).add(scope => {
+      // animate("#title", {
+      //   opacity: [0, 1],
+      //   translateY: ["10%", "0%"],
+      //   duration: 1000,
+      //   easing: "ease",
+      //   delay: 500,
+      // });
+
+      createTimeline()
+        .add("#title", {
+          opacity: [0, 1],
+          translateY: ["10%", "0%"],
+          duration: 1000,
+          easing: "ease",
+          delay: 500,
+        })
+        .add("#subtitle", {
+          opacity: [0, 1],
+          translateY: ["10%", "0%"],
+          duration: 500,
+          easing: "ease",
+        })
+
+    })
+  }, []);
+
   return (
     <>
       <Head>
@@ -24,14 +51,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
-      >
+      <div className={`${styles.page}`} ref={root}>
         <main>
           <CenteredOnPage>
             <Flex direction={"column"} gap="sm" align={"center"}>
-              <Title size={"9rem"}>Your time is limited.</Title>
-              <Text size="xl">A perspective on time, and the way we spend it</Text>
+              <Title id="title" size={"8rem"} opacity={0}>
+                Your time is limited.
+              </Title>
+              <Text id="subtitle" size="xl" opacity={0}>
+                A perspective on time, and the way we spend it
+              </Text>
             </Flex>
           </CenteredOnPage>
         </main>
