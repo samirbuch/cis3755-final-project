4d59d756d6737cf7b83ad9abe7edfa6fdca03830
samirbuch <samir.buch@temple.edu>
2025-04-27 13:01:53 -0400

    ari & jess: set placeholder screen

--- src/pages/jess.tsx
+++ src/pages/jess.tsx
@@ -1,3 +1,22 @@
+import CenteredOnPage from "@/components/CenteredOnPage";
+import { Button, Flex, Text, Title } from "@mantine/core";
+import Link from "next/link";
+import { ClockLoader } from "react-spinners";
+
 export default function Jess() {
-  return (<div></div>)
+  return (
+    <CenteredOnPage>
+      <Flex direction={"column"} align="center" gap="md">
+        <ClockLoader color="#FFF" size={48} />
+        <Title order={2}>Awaiting data...</Title>
+        <Text>Come back later</Text>
+
+        <Link href="/" style={{ marginTop: "1rem" }}>
+          <Button variant="transparent">
+            Try going home
+          </Button>
+        </Link>
+      </Flex>
+    </CenteredOnPage>
+  )
 }
