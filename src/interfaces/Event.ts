e0258e51f228f4b72445b736ba12088e0c7f5bad
samirbuch <samir.buch@temple.edu>
2025-04-30 17:10:15 -0400

    Revert "nix loading of the old format"
    This reverts commit b9847ede736cb216607fef050b09022f676eb0ea.

--- src/interfaces/Event.ts
+++ src/interfaces/Event.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { ZodLinkSourceTargetID } from "./Link";
-import { ZodNodeNoFixed } from "./Node";
+import { ZodNodeNoFixed, OldZodNode } from "./Node";
 
 export const ZodEvent = z.object({
   eventTime: z.object({
@@ -10,7 +10,7 @@ export const ZodEvent = z.object({
   }),
   eventTitle: z.string().nullable(),
   eventDescription: z.string().nullable(),
-  nodes: z.array(ZodNodeNoFixed),
+  nodes: z.array(z.union([ZodNodeNoFixed, OldZodNode])),
   links: z.array(ZodLinkSourceTargetID),
 });
 type ZodDataT = z.infer<typeof ZodEvent>;
